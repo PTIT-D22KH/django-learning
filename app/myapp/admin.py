6dc6f2ea2dca856c7266973ae1853dbf1b2e7ff7
duongvu4 <vucongtuanduong4@gmail.com>
2024-08-22 07:33:22 +0000

    add files

--- app/myapp/admin.py
+++ app/myapp/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from . models import Product
+from . models import Product, MyModel, Offers
 
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'stock')
+    
+
+class MyModelAdmin(admin.ModelAdmin):
+    list_display = ('field1', 'field2', 'field3')
 
 admin.site.register(Product,ProductAdmin)
+admin.site.register(MyModel, MyModelAdmin)
