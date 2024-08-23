36c0135cfc105257de8e3706bd9cd035afe7695f
duongvu4 <vucongtuanduong4@gmail.com>
2024-08-23 14:49:33 +0000

    add files

--- newspaper_app/newspaper/accounts/admin.py
+++ newspaper_app/newspaper/accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = [
+        "username",
+        'email',
+        'age',
+        'is_staff',
+        'job',
+    ]
+    list_filter = [
+        'is_staff',
+    ]
+    search_fields = [
+        'username',
+        'email',
+        'age',
+    ]
+    ordering = ['username',]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age', 'job')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age','job')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
